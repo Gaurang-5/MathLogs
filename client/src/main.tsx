4bdfe1ce9dcd3536cc0be7ce91b86d678fd23681
Gaurang Bhatia <gaurang525@outlook.com>
2026-04-02 04:26:35 +0530

    chore: remove PostHog analytics (broken CSP, spamming console)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { initializeSentry } from './monitoring/sentry'
-import { initializePostHog } from './monitoring/posthog'
 
 // Initialize error monitoring FIRST
 initializeSentry();
